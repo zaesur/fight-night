b11c4fa7feb84c2f1fffde3aca6f0b05b674c0c7
Florian <flo@sitecore.com>
2024-01-13 14:32:52 +0100

    Add documentation to HTTP client

--- src/presentation/http.js
+++ src/presentation/http.js
@@ -1,10 +1,27 @@
 export default class Client {
+    /**
+     * Creates an instance of Client to make requests to the voting API.
+     * @param { string | URL } baseUrl The base URL of the API
+     * @throws TypeError when not given a valid URL
+     * @memberof Client
+     */
     constructor(baseUrl) {
         this.baseUrl = new URL(baseUrl)
     }
 
+    /**
+     * Creates a complete URL given a relative path.
+     * May be used to generate API routes.
+     * @param { string | URL } path The relative path to the resource
+     * @memberof Client
+     */
     getUrl = path => new URL(path, this.baseUrl)
 
+    /**
+     * Requests the current state of the voting system.
+     * @returns { Promise<Response> } A response object
+     * @memberof Client
+     */
     getState = () => fetch(
         this.getUrl("api/state"),
         {
@@ -16,6 +33,13 @@ export default class Client {
         }
     )
 
+    /**
+     * Requests to start the voting system.
+     * @param { number } min Starting id of voting hardware
+     * @param { number } max Ending id of the voting hardware
+     * @returns { Promise<Response> } A response object
+     * @memberof Client
+     */
     startHardware = (min, max) => fetch(
         this.getUrl("api/hardware/start"),
         {
@@ -31,6 +55,11 @@ export default class Client {
         }
     )
 
+    /**
+     * Requests to stop the voting system.
+     * @returns { Promise<Response> } A response object
+     * @memberof Client
+     */
     stopHardware = () => fetch(
         this.getUrl("api/hardware/stop"),
         {
@@ -42,6 +71,13 @@ export default class Client {
         }
     )
 
+
+    /**
+     * Requests the voting sytem to start of a new question.
+     * @param { number } answersCount The number of available answers
+     * @returns { Promise<Response> } A response object
+     * @memberof Client
+     */
     startQuestion = (answersCount) => fetch(
         this.getUrl("api/question/start"),
         {
@@ -56,6 +92,11 @@ export default class Client {
         }
     )
 
+    /**
+     * Requests the voting system to close the question.
+     * @returns { Promise<Response> } A response object
+     * @memberof Client
+     */
     stopQuestion = () => fetch(
         this.getUrl("api/question/start"),
         {
@@ -67,6 +108,12 @@ export default class Client {
         }
     )
 
+    /**
+     * Requests the current results from the voting system,
+     * without closing the question.
+     * @returns { Promise<Response> } A response object
+     * @memberof Client
+     */
     getResults = () => fetch(
         this.getUrl("api/question/results"),
         {
