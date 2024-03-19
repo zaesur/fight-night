4c64c86ee5ad632cd22f601bb3fe4d654b797ee5
Florian <flo@sitecore.com>
2024-03-19 13:54:09 +0100

    Refactor audience

--- src/js/audience.js
+++ src/js/audience.js
@@ -1,8 +1,3 @@
-const resultTemplateElement = document.getElementById("result-template");
-const optionTemplateElement = document.getElementById("option-template");
-const bodyElement = document.querySelector("body");
-const mainElement = document.querySelector("main");
-
 const integerRangeRegex = /(?<=\d{3,})(-|—)(?=\d{3,})/;
 
 const formatOptionId = (optionId) =>
@@ -16,152 +11,166 @@ const formatOptionLabel = (name) => {
   return match ? `${name.slice(0, match.index)}<br>${name.slice(match.index)}` : name.split(" ").join("<br>");
 };
 
-const renderBlank = ({ backgroundColor }) => {
-  bodyElement.style.backgroundColor = backgroundColor;
-  bodyElement.style.visibility = "hidden";
-};
+const Audience = {
+  $: {
+    main: document.querySelector("main"),
+    body: document.querySelector("body"),
+    document: document.documentElement,
+    resultTemplate: document.getElementById("result-template"),
+    optionTemplate: document.getElementById("option-template"),
+  },
 
-const renderOptions = ({ options, showQuestion, question, showOnlyOptionId }) => {
-  bodyElement.style.backgroundColor = "white";
-  bodyElement.style.visibility = "visible";
-  mainElement.classList.remove("center", "results");
-  mainElement.classList.add("options");
+  renderBlank({ backgroundColor }) {
+    Audience.$.body.style.backgroundColor = backgroundColor;
+    Audience.$.body.style.visibility = "hidden";
+  },
 
-  const renderOption = ({ optionId, optionName }) => {
-    const clone = document.importNode(optionTemplateElement.content, true);
-    const labelNode = clone.querySelector(".option-label");
+  renderOptions({ options, showQuestion, question, showOnlyOptionId }) {
+    Audience.$.body.style.backgroundColor = "white";
+    Audience.$.body.style.visibility = "visible";
+    Audience.$.main.classList.remove("center", "results");
+    Audience.$.main.classList.add("options");
 
-    labelNode.innerHTML = showOnlyOptionId ? formatOptionId(optionId) : `${formatOptionId(optionId)} ${optionName}`;
+    const renderOption = ({ optionId, optionName }) => {
+      const clone = document.importNode(Audience.$.optionTemplate.content, true);
+      const labelNode = clone.querySelector(".option-label");
 
-    return clone;
-  };
+      labelNode.innerHTML = showOnlyOptionId ? formatOptionId(optionId) : `${formatOptionId(optionId)} ${optionName}`;
 
-  const questionElement = document.createElement("label");
-  questionElement.textContent = question;
-  questionElement.classList.add("option-question");
+      return clone;
+    };
 
-  mainElement.replaceChildren(...(showQuestion ? [questionElement] : []), ...options.map(renderOption));
-};
+    const questionElement = document.createElement("label");
+    questionElement.textContent = question;
+    questionElement.classList.add("option-question");
 
-const renderResults = ({ options, optionsShown, optionsAnimated, showOnlyOptionId }) => {
-  bodyElement.style.backgroundColor = "white";
-  bodyElement.style.visibility = "visible";
-  mainElement.classList.remove("center", "options");
-  mainElement.classList.add("results");
+    Audience.$.main.replaceChildren(...(showQuestion ? [questionElement] : []), ...options.map(renderOption));
+  },
 
-  const renderResult = ({ optionName, optionId, percentage }) => {
-    const isAnimated = optionsAnimated.includes(optionId);
-    const isVisible = optionsShown.includes(optionId);
-    const isIntegerRange = integerRangeRegex.test(optionName);
+  renderResults({ options, optionsShown, optionsAnimated, showOnlyOptionId }) {
+    Audience.$.body.style.backgroundColor = "white";
+    Audience.$.body.style.visibility = "visible";
+    Audience.$.main.classList.remove("center", "options");
+    Audience.$.main.classList.add("results");
 
-    if (!isVisible) {
-      const div = document.createElement("div");
-      div.classList.add("result-empty");
-      return div;
+    const renderResult = ({ optionName, optionId, percentage }) => {
+      const isAnimated = optionsAnimated.includes(optionId);
+      const isVisible = optionsShown.includes(optionId);
+      const isIntegerRange = integerRangeRegex.test(optionName);
+
+      if (!isVisible) {
+        const div = document.createElement("div");
+        div.classList.add("result-empty");
+        return div;
+      }
+
+      const clone = document.importNode(Audience.$.resultTemplate.content, true);
+
+      const svgNode = clone.querySelector("svg");
+      const circleNode = svgNode.querySelector("circle");
+      const animateNode = svgNode.querySelector("animate");
+      const labelNode = clone.querySelector(".result-label");
+      const percentageNode = clone.querySelector(".result-percentage");
+
+      percentageNode.textContent = `${Math.round(percentage)}%`;
+      labelNode.classList.toggle("number", isIntegerRange);
+      labelNode.innerHTML = showOnlyOptionId ? formatOptionId(optionId) : formatOptionLabel(optionName);
+
+      if (isAnimated) {
+        circleNode.setAttribute("r", 0);
+        animateNode.setAttribute("values", `0;${percentage}`);
+      } else {
+        circleNode.setAttribute("r", percentage);
+        circleNode.removeChild(animateNode);
+      }
+
+      return clone;
+    };
+
+    Audience.$.main.replaceChildren(...options.map(renderResult));
+  },
+
+  renderSummary({ summary }) {
+    Audience.$.body.style.backgroundColor = "white";
+    Audience.$.body.style.visibility = "visible";
+    Audience.$.main.classList.remove("options", "results");
+    Audience.$.main.classList.add("center");
+
+    const element = document.createElement("div");
+    element.classList.add("summary");
+    element.textContent = summary;
+    Audience.$.main.replaceChildren(element);
+  },
+
+  renderVoterIds({ voterIds }) {
+    Audience.$.body.style.backgroundColor = "white";
+    Audience.$.body.style.visibility = "visible";
+    Audience.$.main.classList.remove("options", "results");
+    Audience.$.main.classList.add("center");
+
+    const element = document.createElement("div");
+    element.classList.add("novote");
+    element.textContent = voterIds.map((id) => String(id).padStart(3, "0")).join(", ");
+    mainElement.replaceChildren(element);
+  },
+
+  renderReturnRemotes({ returnRemotes }) {
+    Audience.$.body.style.backgroundColor = "white";
+    Audience.$.body.style.visibility = "visible";
+    Audience.$.main.classList.remove("options", "results");
+    Audience.$.main.classList.add("center");
+
+    const element = document.createElement("div");
+    element.classList.add("return-remotes");
+    element.textContent = returnRemotes;
+    Audience.$.main.replaceChildren(element);
+  },
+
+  render(state, data) {
+    const map = {
+      "showBlank": Audience.renderBlank,
+      "showOptions": Audience.renderOptions,
+      "showResults": Audience.renderResults,
+      "showSummary": Audience.renderSummary,
+      "showVoterIds": Audience.renderVoterIds,
+      "showReturnRemotes": Audience.renderReturnRemotes,
+    };
+
+    // Necessary to show animations correctly.
+    window.requestAnimationFrame(() => {
+      map[state]?.(data);
+    });
+  },
+
+  init() {
+    // Since render is deterministic we can render on every event.
+    window.addEventListener("storage", (event) => {
+      if (event.newValue && event.key === "audience_state") {
+        const { state, data } = JSON.parse(event.newValue);
+        Audience.render(state, data);
+      }
+    });
+
+    // Fullscreen needs to be triggered by a user event.
+    document.addEventListener("click", () => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        Audience.$.document.requestFullscreen();
+      }
+    });
+
+    // Hide the mouse in fullscreen, else show a magnifying glass.
+    document.addEventListener("fullscreenchange", () => {
+      Audience.$.document.style.cursor = document.fullscreenElement ? "none" : "zoom-in";
+    });
+
+    // Initialize on first load.
+    if ("audience_state" in localStorage) {
+      const { state, data } = JSON.parse(window.localStorage.getItem("audience_state"));
+      Audience.render(state, data);
     }
-
-    const clone = document.importNode(resultTemplateElement.content, true);
-
-    const svgNode = clone.querySelector("svg");
-    const circleNode = svgNode.querySelector("circle");
-    const animateNode = svgNode.querySelector("animate");
-    const labelNode = clone.querySelector(".result-label");
-    const percentageNode = clone.querySelector(".result-percentage");
-
-    percentageNode.textContent = `${Math.round(percentage)}%`;
-    labelNode.classList.toggle("number", isIntegerRange);
-    labelNode.innerHTML = showOnlyOptionId ? formatOptionId(optionId) : formatOptionLabel(optionName);
-
-    if (isAnimated) {
-      circleNode.setAttribute("r", 0);
-      animateNode.setAttribute("values", `0;${percentage}`);
-    } else {
-      circleNode.setAttribute("r", percentage);
-      circleNode.removeChild(animateNode);
-    }
-
-    return clone;
-  };
-
-  mainElement.replaceChildren(...options.map(renderResult));
+  },
 };
 
-const renderSummary = ({ summary }) => {
-  bodyElement.style.backgroundColor = "white";
-  bodyElement.style.visibility = "visible";
-  mainElement.classList.remove("options", "results");
-  mainElement.classList.add("center");
-
-  const element = document.createElement("div");
-  element.classList.add("summary");
-  element.textContent = summary;
-  mainElement.replaceChildren(element);
-};
-
-const renderVoterIds = ({ voterIds }) => {
-  bodyElement.style.backgroundColor = "white";
-  bodyElement.style.visibility = "visible";
-  mainElement.classList.remove("options", "results");
-  mainElement.classList.add("center");
-
-  const element = document.createElement("div");
-  element.classList.add("novote");
-  element.textContent = voterIds.map((id) => String(id).padStart(3, "0")).join(", ");
-  mainElement.replaceChildren(element);
-};
-
-const renderReturnRemotes = ({ returnRemotes }) => {
-  bodyElement.style.backgroundColor = "white";
-  bodyElement.style.visibility = "visible";
-  mainElement.classList.remove("options", "results");
-  mainElement.classList.add("center");
-
-  const element = document.createElement("div");
-  element.classList.add("return-remotes");
-  element.textContent = returnRemotes;
-  mainElement.replaceChildren(element);
-};
-
-const render = (state, data) => {
-  const map = {
-    "showBlank": renderBlank,
-    "showOptions": renderOptions,
-    "showResults": renderResults,
-    "showSummary": renderSummary,
-    "showVoterIds": renderVoterIds,
-    "showReturnRemotes": renderReturnRemotes,
-  };
-
-  // Necessary to show animations correctly.
-  window.requestAnimationFrame(() => {
-    map[state]?.(data);
-  });
-};
-
-// Since render is deterministic we can render on every event.
-window.addEventListener("storage", (event) => {
-  if (event.newValue && event.key === "audience_state") {
-    const { state, data } = JSON.parse(event.newValue);
-    render(state, data);
-  }
-});
-
-// Fullscreen needs to be triggered by a user event.
-document.addEventListener("click", () => {
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    document.documentElement.requestFullscreen();
-  }
-});
-
-// Hide the mouse in fullscreen, else show a magnifying glass.
-document.addEventListener("fullscreenchange", () => {
-  document.documentElement.style.cursor = document.fullscreenElement ? "none" : "zoom-in";
-});
-
-// Initialize on first load.
-if ("audience_state" in localStorage) {
-  const { state, data } = JSON.parse(window.localStorage.getItem("audience_state"));
-  render(state, data);
-}
+Audience.init();
